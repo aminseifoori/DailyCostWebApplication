208f2589ff5d86589b188a57e538d3acaf7059f2
Amin <amin.seifoori@gmail.com>
2023-01-14 10:33:46 -0800

    Session 60

---
+++ DailyCostWebApplication/Controllers/UserManagmentController.cs
@@ -0,0 +1,46 @@
+﻿using DailyCostWebApplication.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DailyCostWebApplication.Controllers
+{
+    public class UserManagmentController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManger;
+
+        public UserManagmentController(UserManager<IdentityUser> _userManger)
+        {
+            userManger = _userManger;
+        }
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserViewModel createUserViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = 
+                    new IdentityUser { UserName = createUserViewModel.Email, Email = createUserViewModel.Email };
+                var result = await userManger.CreateAsync(user, createUserViewModel.Password);
+                if (result.Succeeded)
+                {
+                    ViewBag.RegisteredResult = "Yes";
+                    ModelState.Clear();
+                }
+                else
+                {
+                    ViewBag.RegisteredResult = "No";
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError("Validation", error.Description);
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ DailyCostWebApplication/ViewModels/CreateUserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace DailyCostWebApplication.ViewModels
+{
+    public class CreateUserViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [DisplayName("Confirm Password")]
+        [Compare("Password", ErrorMessage = "The Confirm Password Does Not Match With Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
