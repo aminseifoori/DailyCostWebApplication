e3949f168a6cfa9a4fc8247fb33985c81060735e
Amin <amin.seifoori@gmail.com>
2023-01-22 13:30:36 -0800

    Session 61
    SignInManager
    Login and Sign Out Feature
    Inject SignInManager to View

--- DailyCostWebApplication/Controllers/UserManagmentController.cs
+++ DailyCostWebApplication/Controllers/UserManagmentController.cs
@@ -8,10 +8,12 @@ namespace DailyCostWebApplication.Controllers
     public class UserManagmentController : Controller
     {
         private readonly UserManager<IdentityUser> userManger;
+        private readonly SignInManager<IdentityUser> signInManager;
 
-        public UserManagmentController(UserManager<IdentityUser> _userManger)
+        public UserManagmentController(UserManager<IdentityUser> _userManger, SignInManager<IdentityUser> _signInManager)
         {
             userManger = _userManger;
+            signInManager = _signInManager;
         }
         [HttpGet]
         public IActionResult CreateUser()
@@ -41,6 +43,34 @@ namespace DailyCostWebApplication.Controllers
                 }
             }
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInViewModel signIn)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(signIn.Email, signIn.Password, signIn.RememberMe, false);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "UserName or Password is incorrect");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignOut()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ DailyCostWebApplication/ViewModels/SignInViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace DailyCostWebApplication.ViewModels
+{
+    public class SignInViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DisplayName("Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
